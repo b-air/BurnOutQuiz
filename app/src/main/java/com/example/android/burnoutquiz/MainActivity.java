d50b3fac330c9275b78d1b159c2b85e6757e20e9
bruno <scarred.eye@gmail.com>
2017-04-11 13:45:36 +0200

    Creating Ui Styles Welcome / Name box

--- app/src/main/java/com/example/android/burnoutquiz/MainActivity.java
+++ app/src/main/java/com/example/android/burnoutquiz/MainActivity.java
@@ -1,7 +1,19 @@
 package com.example.android.burnoutquiz;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import static com.example.android.burnoutquiz.R.id.checkAnonymous;
+import static com.example.android.burnoutquiz.R.id.nameField;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +22,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Create anonymous behavior
+     */
+
+    public void makeAnon(View view){
+
+        // define var needed
+        final CheckBox checkBox = (CheckBox)findViewById(R.id.checkAnonymous);
+        EditText nameField = (EditText)findViewById(R.id.nameField);
+        TextView txtView = (TextView)findViewById(R.id.nameField);
+
+
+        if(checkBox.isChecked()){
+
+            //change text hide text
+            nameField.setText("Anonymous");
+            txtView.setVisibility(View.GONE);
+
+            //change box color
+            findViewById(R.id.nameCase).setBackgroundColor(Color.parseColor("#F5F5F5"));
+            ((CheckBox) findViewById(R.id.checkAnonymous)).setTextColor(Color.parseColor("#e62831"));
+
+            // hide keyboard
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+        } else {
+
+            // Ui is back
+            nameField.setText("");
+            txtView.setVisibility(View.VISIBLE);
+            findViewById(R.id.nameCase).setBackgroundColor(Color.parseColor("#e62831"));
+            ((CheckBox) findViewById(R.id.checkAnonymous)).setTextColor(Color.parseColor("#F5F5F5"));
+
+        }
+
+
+
+
+
+    }
+
+
 }
